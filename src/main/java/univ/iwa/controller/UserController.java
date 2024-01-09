7b22f22958998baa342d614fccdc83535aa325ed
SoumayaTa <94203643+SoumayaTa@users.noreply.github.com>
2024-01-09 16:14:24 +0100

    jwt commit

---
+++ src/main/java/univ/iwa/controller/UserController.java
@@ -0,0 +1,54 @@
+package univ.iwa.controller;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import univ.iwa.model.AuthRequest;
+import univ.iwa.model.UserInfo;
+import univ.iwa.service.JwtService;
+import univ.iwa.service.UserInfoService;
+@RestController
+@RequestMapping("/auth") 
+public class UserController { 
+    @Autowired UserInfoService service; 
+    @Autowired JwtService jwtService; 
+    @Autowired AuthenticationManager authenticationManager; 
+    @GetMapping("/welcome") 
+    public String welcome() {return "Welcome this endpoint is not secure";}
+
+    @PostMapping("/addNewUser")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String addNewUser(@RequestBody UserInfo format) {
+        return service.addUser(format);
+    } 
+
+    @GetMapping("/assistant/assistantProfile")
+    @PreAuthorize("hasAuthority('ROLE_ASSISTANT')")
+    public String userProfile() { return "Welcome to Assistant Profile"; }
+    @GetMapping("/admin/adminProfile") 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')") 
+    public String adminProfile() { return "Welcome to Admin Profile"; }
+
+    @GetMapping("/format/formatProfile")
+    @PreAuthorize("hasAuthority('ROLE_FORMAT')")
+    public String formatProfile() { return "Welcome to instructor Profile"; }
+
+    @PostMapping("/generateToken") 
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) { 
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())); 
+        if (authentication.isAuthenticated()) { 
+            return jwtService.generateToken(authRequest.getUsername()); 
+        } else { 
+            throw new UsernameNotFoundException("invalid user request !"); 
+        } 
+    }
+
+} 
---
+++ src/main/java/univ/iwa/service/UserInfoService.java
@@ -0,0 +1,57 @@
+package univ.iwa.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails; 
+import org.springframework.security.core.userdetails.UserDetailsService; 
+import org.springframework.security.core.userdetails.UsernameNotFoundException; 
+import org.springframework.security.crypto.password.PasswordEncoder; 
+import org.springframework.stereotype.Service;
+
+import univ.iwa.model.UserInfo;
+import univ.iwa.model.UserInfoDetails;
+import univ.iwa.repository.UserInfoRepository;
+
+import java.util.Optional; 
+
+@Service
+public class UserInfoService implements UserDetailsService { 
+	@Autowired UserInfoRepository repository; 
+	@Autowired PasswordEncoder encoder; 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { 
+		Optional<UserInfo> userDetail = repository.findByName(username); 
+
+		return userDetail.map(UserInfoDetails::new) 
+				.orElseThrow(() -> new UsernameNotFoundException("User not found " + username)); 
+	} 
+	public String addUser(UserInfo format) {
+		format.setName(format.getName());
+		format.setPassword(encoder.encode(format.getPassword()));
+		format.setEmail(format.getEmail());
+		format.setRoles("ROLE_FORMAT");
+		repository.save(format);
+		return "User Added Successfully"; 
+	}
+	@PostConstruct
+	public String adminDefault(){
+		UserInfo admin = new UserInfo();
+		admin.setId(1);
+		admin.setName("admin");
+		admin.setRoles("ROLE_ADMIN");
+		admin.setPassword(encoder.encode("adminadmin"));
+		repository.save(admin);
+		return "admin added successfully";
+	}
+	@PostConstruct
+	public String assistantDefault(){
+		UserInfo assistant = new UserInfo();
+		assistant.setId(2);
+		assistant.setName("assistant1");
+		assistant.setPassword(encoder.encode("assistant1"));
+		assistant.setRoles("ROLE_ASSISTANT");
+		repository.save(assistant);
+		return "assistant added successfully";
+	}
+
+} 
