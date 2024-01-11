bf3c6e504ff607f9e883459d3e079c9bb88f5e65
elberrariwiam2001 <wiam.elberrari@etu.uae.ac.ma>
2024-01-11 15:48:06 +0100

    new formateur added

--- frontend/src/app/shared/services/user.service.ts
+++ frontend/src/app/shared/services/user.service.ts
@@ -1,19 +1,35 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { UserAuthService } from "./user-auth.service";
+import { Observable } from "rxjs";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  private API_BASE_URL = "http://localhost:9096/auth";
-
+  
   constructor(
     private httpClient: HttpClient,
+    private http: HttpClient,
     private userAuthService: UserAuthService
   ) { }
+  private API_BASE_URL = "http://localhost:9096/auth";
+  addUser(user: any): Observable<any> {
+
+    const token = this.userAuthService.getToken();
+    
+ // Assuming you have a method to get the token
+
+    // Set the token in the request header
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.post(`${this.API_BASE_URL}/addNewUser`, user,{headers});
+  }
+
+ 
 
 
   requestHeader=new HttpHeaders(
@@ -36,6 +52,7 @@ export class UserService {
   public forAssistant() {
     return this.httpClient.get(this.API_BASE_URL + '/assistant/assistantProfile', { responseType: "text" });
   }
+  
 
   // public roleMatch(allowedRoles: string[]): boolean {
   //   const userRoles: string = this.userAuthService.getRoles();
